d144bbf44ea8fd487d01b69d2f4216f645b0f21e
Yaroslav Sumiatin <yaroslav_sumiatin@epam.com>
2022-06-06 00:00:37 +0300

    created markup for components

--- src/index.js
+++ src/index.js
@@ -1,45 +1,6 @@
-class SwapiService {
-	_apiBase = "https://swapi.dev/api";
+import { createRoot } from "react-dom/client";
+import App from "./components/app";
 
-	async getResource(url) {
-		const resource = await fetch(`${this._apiBase}${url}`);
-		if (!resource.ok) {
-			throw new Error(
-				`Could not fetch ${url}. Received ${resource.status} code.`
-			);
-		}
-		return await resource.json();
-	}
-
-	async getAllPeople() {
-		const response = await this.getResource(`/people/`);
-		return response.results;
-	}
-
-	getPerson(id) {
-		return this.getResource(`/people/${id}/`);
-	}
-
-	async getAllPlanets() {
-		const response = await this.getResource(`/planets/`);
-		return response.results;
-	}
-
-	getPlanet(id) {
-		return this.getResource(`/planets/${id}/`);
-	}
-
-	async getAllStarships() {
-		const response = await this.getResource(`/starships/`);
-		return response.results;
-	}
-
-	getStarship(id) {
-		return this.getResource(`/starships/${id}/`);
-	}
-}
-
-const swapi = new SwapiService();
-swapi.getStarship(1).then((person) => {
-	console.log(person.name);
-});
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(<App />);
---
+++ src/services/swapi-service.js
@@ -0,0 +1,40 @@
+export default class SwapiService {
+	_apiBase = "https://swapi.dev/api";
+
+	async getResource(url) {
+		const resource = await fetch(`${this._apiBase}${url}`);
+		if (!resource.ok) {
+			throw new Error(
+				`Could not fetch ${url}. Received ${resource.status} code.`
+			);
+		}
+		return await resource.json();
+	}
+
+	async getAllPeople() {
+		const response = await this.getResource(`/people/`);
+		return response.results;
+	}
+
+	getPerson(id) {
+		return this.getResource(`/people/${id}/`);
+	}
+
+	async getAllPlanets() {
+		const response = await this.getResource(`/planets/`);
+		return response.results;
+	}
+
+	getPlanet(id) {
+		return this.getResource(`/planets/${id}/`);
+	}
+
+	async getAllStarships() {
+		const response = await this.getResource(`/starships/`);
+		return response.results;
+	}
+
+	getStarship(id) {
+		return this.getResource(`/starships/${id}/`);
+	}
+}
